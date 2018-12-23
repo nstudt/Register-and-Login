5afedd564d3020b4e9f3dbe5de2b41ba8b106032
nstudt <nstudt@gmail.com>
2018-12-23 16:55:57 +0800

    change to index.php

--- index.php
+++ index.php
@@ -19,15 +19,10 @@ if (isset($_SESSION['userLoggedIn'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="includes/assets/css/style.css">
-    <title>Welcome to Xamppify</title>
+    <title>Registration and Login</title>
 </head>
 <body>
-    <div id="nowPlayingBarContainer">
-
-        <div id="nowPlayingBar">
-
-        </div>
-        
-    </div>
+    <h1>Generic Index Page</h1>
+    <h2>Goto <a href="register.php"></a>register.php</h2>
 </body>
 </html>
